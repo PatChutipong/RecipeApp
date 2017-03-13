90523c07a0a6323ed7b83ed44ec5e0070313ff1c
Pat Chutipong <patchutipong@Pats-MacBook-Pro.local>
2017-03-12 22:25:38 -0500

    Initial commit

---
+++ app/src/main/java/com/example/patchutipong/recipeapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.patchutipong.recipeapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import java.util.ArrayList;
+import android.widget.ListView;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> arrayList;
+    private ArrayAdapter<String> adapter;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu);
+        ListView listViewHandler=(ListView)findViewById(R.id.usefulList);
+        String[] menuItems={"menuItem 1","menuItem 2","menuItem 3","menuItem 1","menuItem 2","menuItem 3","menuItem 1","menuItem 2","menuItem 3","menuItem 1","menuItem 2","menuItem 3"};
+        arrayList = new ArrayList<>(Arrays.asList(menuItems));
+        adapter = new ArrayAdapter<String>(this,R.layout.listitem,R.id.textItem,arrayList);
+        listViewHandler.setAdapter(adapter);
+
+
+    }
+}
