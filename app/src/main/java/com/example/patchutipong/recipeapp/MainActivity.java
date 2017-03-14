b4641d1cbe28afd1f865e98a2e10bddcb4a2cb33
PatChutipong <pat.chutipong@gmail.com>
2017-03-13 21:56:44 -0500

    sick ass codez

--- app/src/main/java/com/example/patchutipong/recipeapp/MainActivity.java
+++ app/src/main/java/com/example/patchutipong/recipeapp/MainActivity.java
@@ -2,25 +2,18 @@ package com.example.patchutipong.recipeapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import java.util.ArrayList;
-import android.widget.ListView;
-import java.util.Arrays;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ArrayList<String> arrayList;
-    private ArrayAdapter<String> adapter;
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.menu);
-        ListView listViewHandler=(ListView)findViewById(R.id.usefulList);
-        String[] menuItems={"menuItem 2","menuItem 3","menuItem 1","menuItem 2","menuItem 3","menuItem 1","menuItem 2","menuItem 3","menuItem 1","menuItem 2","menuItem 3"};
-        arrayList = new ArrayList<>(Arrays.asList(menuItems));
-        adapter = new ArrayAdapter<String>(this,R.layout.listitem,R.id.textItem,arrayList);
-        listViewHandler.setAdapter(adapter);
-
-
+        setContentView(R.layout.homescreen);
     }
+
+    public void navigate(View view){
+        Intent move = new Intent(this,);
+    }
+
 }
---
+++ app/src/main/java/com/example/patchutipong/recipeapp/test.java
@@ -0,0 +1,8 @@
+package com.example.patchutipong.recipeapp;
+
+/**
+ * Created by patchutipong on 3/13/17.
+ */
+
+public class test {
+}
